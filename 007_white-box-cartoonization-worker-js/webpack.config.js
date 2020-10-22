7762d6395b49f670d092c13cfc2a52a7c13c9c9c
wataru okada <wataru.okada@flect.co.jp>
2020-10-22 15:40:10 +0900

    hoge

---
+++ 007_white-box-cartoonization-worker-js/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const WorkerPlugin = require('worker-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/white-box-cartoonization-worker.ts', // <-- (1)
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+        ],
+    },
+    output: {
+        filename: 'white-box-cartoonization-worker.js', // <-- (2)
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        globalObject: 'typeof self !== \'undefined\' ? self : this'
+    },
+    plugins: [　　　　　　　　　　　　　　　　 // <--- (2)
+        new WorkerPlugin()
+    ]
+};
