8ac2bab2ac12c19d0072e4ca1468cc4a5f47964c
wataru okada <wataru.okada@flect.co.jp>
2020-11-13 06:06:59 +0900

    enable change worker path

--- 003_ascii-worker-js/webpack.config.js
+++ 003_ascii-worker-js/webpack.config.js
@@ -1,8 +1,30 @@
 const path = require('path');
 const WorkerPlugin = require('worker-plugin');
-module.exports = {
+// module.exports = {
+//     mode: 'development',
+//     entry: './src/asciiart-worker.ts', 
+//     resolve: {
+//         extensions: [".ts", ".js"],
+//     },
+//     module: {
+//         rules: [
+//             { test: /\.ts$/, loader: 'ts-loader' },
+//         ],
+//     },
+//     output: {
+//         filename: 'asciiart-worker.js', // <-- (2)
+//         path: path.resolve(__dirname, 'dist'),
+//         libraryTarget: 'umd',
+//         globalObject: 'typeof self !== \'undefined\' ? self : this'
+//     },
+//     plugins: [　　　　　　　　　　　　　　　　 // <--- (2)
+//         new WorkerPlugin()
+//     ]
+// };
+
+const main = {
     mode: 'development',
-    entry: './src/asciiart-worker.ts', // <-- (1)
+    entry: './src/asciiart-worker.ts', 
     resolve: {
         extensions: [".ts", ".js"],
     },
@@ -22,3 +44,28 @@ module.exports = {
     ]
 };
 
+const worker = {
+    mode: 'development',
+    entry: './src/asciiart-worker-worker.ts', 
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+        ],
+    },
+    output: {
+        filename: 'asciiart-worker-worker.js', // <-- (2)
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        globalObject: 'typeof self !== \'undefined\' ? self : this'
+    },
+    plugins: [　　　　　　　　　　　　　　　　 // <--- (2)
+        new WorkerPlugin()
+    ]
+};
+
+module.exports = [
+    main, worker
+];
