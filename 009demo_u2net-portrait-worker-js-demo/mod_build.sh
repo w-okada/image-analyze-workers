5b339618f458ee6e5c5bc704356a630fb9923b35
wataru okada <wataru.okada@flect.co.jp>
2020-12-14 01:38:06 +0900

    update

---
+++ 009demo_u2net-portrait-worker-js-demo/mod_build.sh
@@ -0,0 +1,4 @@
+cd ../009_u2net-portrait-worker-js; npm run build; cd -;\
+ cp ../009_u2net-portrait-worker-js/dist/* node_modules/\@dannadori/u2net-portrait-worker-js/dist/; \
+ cp node_modules/\@dannadori/u2net-portrait-worker-js/dist/u2net-portrait-worker-worker.js public/;
+
