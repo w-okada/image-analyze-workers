df52121b03aebf9fb8d1fc3be3b627f8da7eeb69
wok <wataru.okada@flect.co.jp>
2021-01-09 07:14:35 +0900

    update browser jbf

--- 011demo_googlemeet-segmentation-worker-js-demo/sync.sh
+++ 011demo_googlemeet-segmentation-worker-js-demo/sync.sh
@@ -1,12 +1,8 @@
 aws s3 sync build/ s3://flect-lab-web/P01_wokers/t11_googlemeet-segmentation
-aws s3 sync build/googlemeet-segmentation_128_16/ s3://flect-lab-web/googlemeet-segmentation_128_16
-aws s3 sync build/googlemeet-segmentation_128_32/ s3://flect-lab-web/googlemeet-segmentation_128_32
-aws s3 sync build/googlemeet-segmentation_144_16/ s3://flect-lab-web/googlemeet-segmentation_144_16
-aws s3 sync build/googlemeet-segmentation_144_32/ s3://flect-lab-web/googlemeet-segmentation_144_32
-aws s3 sync build/googlemeet-segmentation_96_16/ s3://flect-lab-web/googlemeet-segmentation_96_16
-aws s3 sync build/googlemeet-segmentation_96_32/ s3://flect-lab-web/googlemeet-segmentation_96_32
-# aws s3 sync build/u2net-portrait_256/ s3://flect-lab-web/u2net-portrait_256
-# aws s3 sync build/u2net-portrait_320/ s3://flect-lab-web/u2net-portrait_320
-# aws s3 sync build/u2net-portrait_512/ s3://flect-lab-web/u2net-portrait_512
-# aws s3 sync build/u2net-portrait_1024/ s3://flect-lab-web/u2net-portrait_1024
+# aws s3 sync build/googlemeet-segmentation_128_16/ s3://flect-lab-web/googlemeet-segmentation_128_16
+# aws s3 sync build/googlemeet-segmentation_128_32/ s3://flect-lab-web/googlemeet-segmentation_128_32
+# aws s3 sync build/googlemeet-segmentation_144_16/ s3://flect-lab-web/googlemeet-segmentation_144_16
+# aws s3 sync build/googlemeet-segmentation_144_32/ s3://flect-lab-web/googlemeet-segmentation_144_32
+# aws s3 sync build/googlemeet-segmentation_96_16/ s3://flect-lab-web/googlemeet-segmentation_96_16
+# aws s3 sync build/googlemeet-segmentation_96_32/ s3://flect-lab-web/googlemeet-segmentation_96_32
 
