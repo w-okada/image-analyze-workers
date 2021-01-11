c07b73819c0231e3790b900d6bc5baa2fc7613da
wok <wataru.okada@flect.co.jp>
2021-01-12 01:30:29 +0900

    wasm alpha

--- 011_googlemeet-segmentation-worker-js/crate/src/lib.rs
+++ 011_googlemeet-segmentation-worker-js/crate/src/lib.rs
@@ -1,123 +1,146 @@
 extern crate wasm_bindgen;
-use std::sync::{Once,};
 use wasm_bindgen::prelude::*;
-use rand::Rng;
-use chrono::{DateTime, Local};
-use std::convert::TryFrom;
 
-#[derive(Clone)]
+use std::{sync::Mutex};
+use once_cell::sync::Lazy;
+use std::f64::consts::PI;
+//use std::f32::consts::PI;
+
+#[wasm_bindgen]
+extern "C"{
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
+}
+
+#[wasm_bindgen]
+pub fn shared_memory() -> JsValue {
+    wasm_bindgen::memory()
+}
+
 pub struct JointBilateralFilter2 {
-    w: u32,
-    h: u32,
-    sp: u32,
-    range: u32,
     //https://github.com/rustwasm/wasm-bindgen/issues/1848#issuecomment-549855068
-    src_image: Vec<f64>,
-    seg_image: Vec<f64>,
-    pad_src_image: Vec<f64>,
-    pad_seg_image: Vec<f64>,
-    out_image: Vec<f64>,
+    src_image    : Vec<f64>,
+    seg_image    : Vec<f64>,
+    // pad_src_image: Vec<f64>,
+    // pad_seg_image: Vec<f64>,
+    out_image    : Vec<f64>,
+
+    src_image_i32    : Vec<f32>,
+    seg_image_i32    : Vec<f32>,
+    // pad_src_image_i32: Vec<f32>,
+    // pad_seg_image_i32: Vec<f32>,
+    out_image_i32    : Vec<f32>,
+
+
+    matrix: Vec<f64>,
+    //matrix_f32: Vec<f32>,
 }
 
+static GLOBAL_DATA: Lazy<Mutex<JointBilateralFilter2>> = Lazy::new(|| {
+    let instance = JointBilateralFilter2 {
+        src_image:     vec![0.0; 1024 * 1024 * 1], // w, h , ch 
+        seg_image:     vec![0.0; 1024 * 1024 * 1],
+        // pad_src_image: vec![0.0; 1024 * 1024 * 1],
+        // pad_seg_image: vec![0.0; 1024 * 1024 * 1],
+        out_image:     vec![0.0; 1024 * 1024 * 1],
 
-pub fn get_JointBilateralFilter() -> Box<JointBilateralFilter2>{
-    static mut INSTANCE: Option<Box<JointBilateralFilter2>> = None;
-    static once: Once = Once::new();
-    unsafe{
-        once.call_once(||{
-            let instance = JointBilateralFilter2 {
-                w:1,h:2,sp:3,range:1,
-                src_image:     vec![0.0; 1024 * 1024 * 4 * 4],
-                seg_image:     vec![0.0; 1024 * 1024 * 1 * 4],
-                pad_src_image: vec![0.0; 1024 * 1024 * 1 * 4],
-                pad_seg_image: vec![0.0; 1024 * 1024 * 1 * 4],
-                out_image:     vec![0.0; 1024 * 1024 * 1 * 4],
+        src_image_i32:     vec![0.0; 1024 * 1024 * 1], // w, h , ch 
+        seg_image_i32:     vec![0.0; 1024 * 1024 * 1],
+        // pad_src_image_i32: vec![0.0; 1024 * 1024 * 1],
+        // pad_seg_image_i32: vec![0.0; 1024 * 1024 * 1],
+        out_image_i32:     vec![0.0; 1024 * 1024 * 1],
 
-            };
-            INSTANCE = Some(Box::new(instance));
-        });
+        matrix:            vec![0.0; 256],
+        //matrix_f32:        vec![0.0; 256],
 
-
-        INSTANCE.clone().unwrap()
-    }
-
-}
-
+    };
+    Mutex::new(instance)
+});
 
 #[wasm_bindgen]
 pub fn get_config() -> Vec<u32> {
-    let jbf = get_JointBilateralFilter();
-    vec![jbf.w, jbf.h, jbf.range]
+    let jbf = GLOBAL_DATA.lock().unwrap();
+    let src_image_ptr = jbf.src_image.as_ptr() as u32;
+    let seg_image_ptr = jbf.seg_image.as_ptr() as u32;
+    let out_image_ptr = jbf.out_image.as_ptr() as u32;
+
+    let src_image_i32_ptr = jbf.src_image_i32.as_ptr() as u32;
+    let seg_image_i32_ptr = jbf.seg_image_i32.as_ptr() as u32;
+    let out_image_i32_ptr = jbf.out_image_i32.as_ptr() as u32;
+    
+
+    vec![src_image_ptr, seg_image_ptr, out_image_ptr, 
+        src_image_i32_ptr, seg_image_i32_ptr, out_image_i32_ptr,
+    ]
+
 }
+
+#[wasm_bindgen]
+pub fn do_filter(w:u32, h:u32, sp:u32, range:u32){
+    let mut jbf = GLOBAL_DATA.lock().unwrap();
+    let gaussian_range   = 1.0 / (2.0*PI* (range as f64*range as f64)).sqrt() ;
+    for i in 0..256u32{
+        jbf.matrix[i as usize] = (-1.0*((i as f64*i as f64*gaussian_range) as f64)).exp();
+    }
+
+    for i in sp .. sp+h{
+        for j in sp .. sp+w{
+            let center_position = ( (sp+w+sp) * i + j ) as usize;
+            let center_val = jbf.src_image[center_position];
+            let mut norm = 0.0f64;
+            let mut sum  = 0.0f64;
+            for ki in 0 .. sp*2+1{
+                for kj in 0 .. sp*2+1{
+                    let kernel_position = ((i - sp + ki)*(sp+w+sp) + (j - sp + kj))  as usize;
+                    let index = ((jbf.src_image[kernel_position] - center_val).abs().floor()) as usize;
+                    let val   = jbf.matrix[index];
+                    norm += val;
+                    sum  += jbf.seg_image[kernel_position] * val;
+                }
+            }
+            jbf.out_image[ ((i-sp)*w + (j-sp)) as usize] = sum/norm;
+        }
+    }    
+}
+
+
+
+// #[wasm_bindgen]
+// pub fn do_filter_float32(w:u32, h:u32, sp:u32, range:u32){
+//     let mut jbf = GLOBAL_DATA.lock().unwrap();
+//     let gaussian_range   = 1.0 / (2.0*PI* (range as f32*range as f32)).sqrt() ;
+//     for i in 0..256u32{
+//         jbf.matrix_f32[i as usize] = (-1.0*((i as f32*i as f32*gaussian_range) as f32)).exp();
+//     }
+
+//     let spwsp = sp+w+sp;
+
+//     for i in sp .. sp+h{
+//         for j in sp .. sp+w{
+//             let center_position = ( spwsp * i + j ) as usize;
+//             let center_val = jbf.src_image_i32[center_position];
+//             let mut norm = 0.0f32;
+//             let mut sum  = 0.0f32;
+//             for ki in 0 .. sp*2+1{
+//                 for kj in 0 .. sp*2+1{
+//                     let kernel_position = ((i - sp + ki)*spwsp + (j - sp + kj))  as usize;
+//                     let index = ((jbf.src_image_i32[kernel_position] - center_val).abs().floor()) as usize;
+//                     let val   = jbf.matrix_f32[index];
+//                     norm += val;
+//                     sum  += jbf.seg_image_i32[kernel_position] * val;
+//                 }
+//             }
+//             jbf.out_image_i32[ ((i-sp)*w + (j-sp)) as usize] = sum/norm;
+//         }
+//     }    
+// }
+
 
 
 #[wasm_bindgen(start)]
 pub fn initialize() {
     set_panic_hook();
 }
-
-
-#[wasm_bindgen]
-pub struct JointBilateralFilter {
-    w: u32,
-    h: u32,
-    sp: u32,
-    range: u32,
-
-}
-
-
-#[wasm_bindgen]
-impl JointBilateralFilter{
-    #[wasm_bindgen(constructor)]
-    pub fn new(w:u32, h:u32, sp:u32, range:u32) -> JointBilateralFilter{
-        JointBilateralFilter {w, h, sp, range}
-    }
-
-    #[wasm_bindgen]
-    pub fn get_config(self) -> Vec<u32> {
-        vec![self.w, self.h]
-        // vec![1,2]
-    }
-    
-}
-
-
-#[wasm_bindgen]
-pub fn add(a:u32, b:u32) -> u32 {
-    a+b
-}
-
-
-#[wasm_bindgen]
-pub fn sum1(a:Vec<u32>) -> u32 {
-//    a.iter().fold(0, |sum, b| sum + b)
-    10
-}
-
-
-#[wasm_bindgen]
-pub fn sum2(a:u32) -> Vec<u32> {
-//    a.iter().fold(0, |sum, b| sum + b)
-    vec![1,2,3]
-}
-
-
-#[wasm_bindgen]
-pub fn greeting() -> String{
-    "Hello World!!".to_string()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn it_works() {
-        assert_eq!(greeting(), "hello".to_string());
-    }
-}
-
-
 
 pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
