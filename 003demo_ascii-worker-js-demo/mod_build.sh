2277b301898989a52e080ef71c3cc1dd532f949a
wataru <wataru@gal.local.com>
2021-03-31 21:39:19 +0900

    update

--- 003demo_ascii-worker-js-demo/mod_build.sh
+++ 003demo_ascii-worker-js-demo/mod_build.sh
@@ -1,1 +1,3 @@
-cd ../003_ascii-worker-js; npm run build ;cd - ; cp ../003_ascii-worker-js/dist/* node_modules/\@dannadori/asciiart-worker-js/dist/; cp node_modules/\@dannadori/asciiart-worker-js/dist/0.asciiart-worker.worker.js public/
+cd ../003_ascii-worker-js; npm run build ;cd - ; 
+cp ../003_ascii-worker-js/dist/* node_modules/\@dannadori/asciiart-worker-js/dist/; 
+cp node_modules/\@dannadori/asciiart-worker-js/dist/asciiart-worker-worker.js public/
