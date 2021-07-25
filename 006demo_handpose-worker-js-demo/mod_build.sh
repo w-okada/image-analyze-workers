0a75a74d209c6831ca7d527914f0eefd333fde7b
wataru <wataru.okada@flect.co.jp>
2021-07-25 15:10:57 +0900

    update

--- 006demo_handpose-worker-js-demo/mod_build.sh
+++
@@ -1,3 +0,0 @@
-cd ../006_handpose-worker-js; npm run build; cd -;\
- cp ../006_handpose-worker-js/dist/* node_modules/\@dannadori/handpose-worker-js/dist/; \
- cp node_modules/\@dannadori/handpose-worker-js/dist/handpose-worker-worker.js public/;
--- 006demo_handpose-worker-js-demo/sync.sh
+++
@@ -1,2 +0,0 @@
-aws s3 sync build/ s3://flect-lab-web/P01_wokers/t06_handpose
-
