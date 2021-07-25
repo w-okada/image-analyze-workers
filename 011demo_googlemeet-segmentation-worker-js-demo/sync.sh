c36c1dada517293d0eb0acece45cc7a327a13e35
wataru <wataru.okada@flect.co.jp>
2021-07-25 19:11:05 +0900

    update

--- 011demo_googlemeet-segmentation-worker-js-demo/sync.sh
+++
@@ -1,8 +0,0 @@
-aws s3 sync build/ s3://flect-lab-web/P01_wokers/t11_googlemeet-segmentation
-# aws s3 sync build/googlemeet-segmentation_128_16/ s3://flect-lab-web/googlemeet-segmentation_128_16
-# aws s3 sync build/googlemeet-segmentation_128_32/ s3://flect-lab-web/googlemeet-segmentation_128_32
-# aws s3 sync build/googlemeet-segmentation_144_16/ s3://flect-lab-web/googlemeet-segmentation_144_16
-# aws s3 sync build/googlemeet-segmentation_144_32/ s3://flect-lab-web/googlemeet-segmentation_144_32
-# aws s3 sync build/googlemeet-segmentation_96_16/ s3://flect-lab-web/googlemeet-segmentation_96_16
-# aws s3 sync build/googlemeet-segmentation_96_32/ s3://flect-lab-web/googlemeet-segmentation_96_32
-
