b1860b4eb3acb2291d4bd3b3806615f2857e8f06
wataru <wataru.okada@flect.co.jp>
2021-08-20 10:21:25 +0900

    update

--- 007_white-box-cartoonization-worker-js/webpack.config.js
+++ 007_white-box-cartoonization-worker-js/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const WorkerPlugin = require('worker-plugin');
 
 const manager = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/white-box-cartoonization-worker.ts', // <-- (1)
     resolve: {
         extensions: [".ts", ".js"],
@@ -24,7 +24,7 @@ const manager = {
     ]
 };
 const worker = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/white-box-cartoonization-worker-worker.ts', // <-- (1)
     resolve: {
         extensions: [".ts", ".js"],
