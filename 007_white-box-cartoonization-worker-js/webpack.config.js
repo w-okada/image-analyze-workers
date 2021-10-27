c10e5cd85cb22f993023f1733f7aeb82caf459cb
Wataru Okada <wataru.okada@flect.co.jp>
2021-10-27 20:06:06 +0900

    update

--- 007_white-box-cartoonization-worker-js/webpack.config.js
+++ 007_white-box-cartoonization-worker-js/webpack.config.js
@@ -1,34 +1,37 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
-const manager  = {
-    mode: 'production',
-    entry: './src/white-box-cartoonization-worker.ts',
+const manager = {
+    mode: "production",
+    entry: "./src/white-box-cartoonization-worker.ts",
     resolve: {
         extensions: [".ts", ".js"],
-        fallback: { 
-            "os": false,
-            "buffer":  require.resolve("buffer/")
-        }
+        fallback: {
+            os: false,
+            buffer: require.resolve("buffer/"),
+        },
     },
     module: {
         rules: [
-            { test: /\.ts$/, loader: 'ts-loader'},
-            { test:/resources\/.*\.bin/, type:"asset/inline"},
-            { test:/resources\/.*\.json/, type:"asset/source"}
+            { test: /\.ts$/, loader: "ts-loader" },
+            { test: /resources\/.*\.bin/, type: "asset/inline" },
+            { test: /resources\/.*\.json/, type: "asset/source" },
         ],
-
     },
     output: {
-        filename: 'white-box-cartoonization-worker.js',
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        globalObject: 'typeof self !== \'undefined\' ? self : this'
+        filename: "white-box-cartoonization-worker.js",
+        path: path.resolve(__dirname, "dist"),
+        libraryTarget: "umd",
+        globalObject: "typeof self !== 'undefined' ? self : this",
     },
     stats: {
         children: true,
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ],
 };
 
-module.exports = [
-    manager,
-];
+module.exports = [manager];
